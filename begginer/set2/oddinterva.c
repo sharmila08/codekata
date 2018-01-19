1c870251eb7afc56e7ef60f2d4937d136c28919b
sharmila08 <35565709+sharmila08@users.noreply.github.com>
2018-01-19 16:40:48 +0530

    Create oddinterva.c

---
+++ begginer/set2/oddinterva.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,n,k;
+clrscr();
+printf("enter the value of n and k");
+	scanf("%d %d",&n,&k);
+	for(i=n+1;i<k;i++)
+	{
+		if(i%2!=0)
+		printf("\t%d\t",i);
+	}
+  getch();
+}
