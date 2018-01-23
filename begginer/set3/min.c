3162f81b77e3c3232d3b87a047f06702e7db15d3
sharmila08 <35565709+sharmila08@users.noreply.github.com>
2018-01-23 15:53:19 +0530

    Create min.c

---
+++ begginer/set3/min.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n,temp;
+	int a[1000];
+	printf("enter the number\n");
+	scanf("%d",&n);
+	printf("enter the array of n numbers\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+		for(i=0;i<n;i++)
+		{
+			for(j=i+1;j<n;j++)
+			{
+				if(a[i]>a[j])
+				{
+				temp=a[j];
+				a[j]=a[i];
+				a[i]=temp;
+				}
+			}
+		}
+		printf("the maximum num is %d\t",a[0]);
+	return 0;
+}
