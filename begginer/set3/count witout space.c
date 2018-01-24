14373cd1abf35958b7e6c9c1352873c6c0d47150
sharmila08 <35565709+sharmila08@users.noreply.github.com>
2018-01-24 23:07:10 +0530

    Create count witout space.c

---
+++ begginer/set3/count witout space.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	char a[1000];
+	int i,count=0;
+	printf("enter the string\n");
+	scanf("%[^\n]s",&a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]!='\t')
+		{
+			count++;
+		}
+	}
+	printf("%d",count);
+
+	return 0;
+}
