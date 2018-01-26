09000d64de002ec053c88aa2ced1eec2f09992a5
sharmila08 <35565709+sharmila08@users.noreply.github.com>
2018-01-26 11:47:50 +0530

    Update ap.c

--- begginer/set3/ap.c
+++ begginer/set3/ap.c
@@ -4,7 +4,7 @@ int main()
 	char a[1000];
 	int i,sum=0;
 	int N,A,D;
-	printf("enter the value of N,A,D");
+	printf("enter the value of N,A,D\n");
 	scanf("%d %d %d",&N,&A,&D);
 	while(i<N)
 	{
@@ -14,15 +14,14 @@ int main()
 		}
 		else
 		{
-			a[i]=a[i]+D;
+			a[i]=a[i-1]+D;
 		}	
-		printf("%d",a[i]);
 		i++;
 	}
 	for(i=0;i<N;i++)
 	{
 	sum=sum+a[i];
-	printf("%d",sum);
 	}
+		printf("%d",sum);
 	return 0;
 }
