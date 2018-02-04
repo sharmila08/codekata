413c58ffa3192f589c5e82bb568ca54db26d108b
sharmila08 <35565709+sharmila08@users.noreply.github.com>
2018-02-04 20:50:40 +0530

    Create reversee.c

---
+++ player/set1/reversee.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+char a[100];
+int i,count=0,length;
+printf("enter the string");
+scanf("%s",a);
+for(i=0;a[i]!='\0';i++)
+{
+	count++;
+}
+length=count;
+for(i=length;i>=0;i--)
+{
+printf("%c",a[i]);	
+}
+return 0;
+}
