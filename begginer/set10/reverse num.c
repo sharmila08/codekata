f1c27b8f01e776317e189192cd13b596be0febf1
sharmila08 <35565709+sharmila08@users.noreply.github.com>
2018-02-22 16:15:14 +0530

    Create reverse num.c

---
+++ begginer/set10/reverse num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int rem,num,sum=0;
+	int i;
+	printf("enter the num\n");
+	scanf("%d",&num);
+	i=num;
+	while(num>0)
+	{
+		rem=num%10;
+		num=num/10;
+		sum=(sum*10)+rem;
+	}
+	printf("%d",sum);
+	return 0;
+}
